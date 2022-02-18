9e9204c652bd85b9b35591ba2acc33df1374922a
Denzy7 <dennismyner7@gmail.com>
2022-02-18 07:15:02 +0300

    Initial commit

---
+++ safetyoff.sh
@@ -0,0 +1,18 @@
+ansired="\033[0;31m"
+
+echo -e "${ansired}ARE YOU SURE YOU WANT TO DISABLE SAFETY?\n[y/n]: "
+read disable
+if [ ${disable} = "y" ]
+then
+	SHFILES=$(ls *.bak)
+	for i in ${SHFILES}
+	do
+		SHFILE=${i%.*}
+		cat ${i} | sed 's/#//g' > ${SHFILE}
+		chmod +x ${SHFILE}
+	done
+
+
+	echo "SAFETY IS OFF"
+fi
+
